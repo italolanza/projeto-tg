8dc88f9489faaa36987c0552bb375aeed2c3e68c
Italo LANZA <italo_lanza@live.com>
2024-10-16 22:00:04 -0300

    Alterado estrutura dos arquivos do python_code
    - Alterado o caminho para os arquivos de audio nao processados.
    - Alterado caminho para os scritps python para facilitar o uso
      deles nos notebooks.
    - Adicionado flag __FPU_USED no codigo do stm32 para fazer com que
      o codigo compile corretamente.
    - WIP: criado um novo notebook para simular a forma que as features
      sao extraidas no microcontrolador.
    - Adicionado ao README informacoes de como usar o poetry.

--- python_code/README.md
+++ python_code/README.md
@@ -7,7 +7,42 @@ Scritps:
 + `ExtractFeature.py` -> Adaptacao para Python do script escrito em Matlab do autor do artigo que faz a extracao das features com base em um arquivo de audio;
 + `Teste_converter_audio2wav.ipynb` -> Notebook simples que utiliza a biblioteca `soundfile` para converter um arquivo de audio de extensao `.flac` para `.wav`;
 + `TrainClassifier.ipynb` -> Faz o treinamento e avaliacao da acuracia dos classificadores utilizando um arquivo `.csv` gerado pelo notebook `Main.ipynb`;
-+ `SDCard.py` ->
++ `SDCard.py` -> Faz o treinamento e testa acuracia dos classificadores utilizando um arquivo csv (SDCard.txt) que contem as features extraidas dos audios.
+    + Classificadores utilizados:
+        + `Bagged Trees Ensemble`
+        + `Quadratic SVM`
+        + `Fine Decision Tree`
+        + `Naïve Bayes`
+        + `KNeighbors (KNN)`
++ `FeatureExtractor` -> Notebook que faz a extracao das features da mesma forma que esta implementado no microncontrolador
+
+## Como usar o poetry
+
+- Voce precisa possuir o _poetry_ instalado na sua maquina: `pip install poetry`
+
+### Instalando as dependecias com `poetry`
+
+- Para instalar as dependencias do projeto (`pythonproject.toml`), e necessario rodar o comando
+
+```shell
+poetry install --no-root
+```
+
+### Criando o kernel Jupyter Notebook com o `poetry`
+
+- Para criar um Kernel, execute o comando abaixo:
+
+```shell
+poetry run python -m ipykernel install --user --name="Projeto-tg" --display-name "Kernel TG"
+```
+
+### Abrindo o kernel
+
+- Execute o comando abaixo para comecar a execucao ao servidor Jupyter.
+
+```shell
+poetry run jupyter notebook
+```
 
 ## Link Repositorio Artigo Base
 
--- python_code/tg_utils/ExtractFeatures.py
+++ python_code/tg_utils/ExtractFeatures.py
@@ -73,6 +73,62 @@ def ExtractFeatures(DataBuff, BufferLen, Fs):
             SigCrestFactor, SigShapeFactor, SigImpulseFactor, SigMarginFactor, 
             pks1, pks2, pks3, locs1, locs2, locs3)
 
+
+def ExtractTimeDomainFeatures(DataBuff, BufferLen, Fs):
+    
+    # Cálculo dos parâmetros/features
+    SigRMS = np.sqrt(np.mean(DataBuff**2))
+    SigMean = np.mean(DataBuff)
+    Abs = np.abs(DataBuff)
+    MeanAbs = np.mean(Abs)
+    Max = np.max(DataBuff)
+    SigMedian = np.median(DataBuff)
+    SigVariance = np.var(DataBuff)
+    SigSkewness = skew(DataBuff)
+    SigKurtosis = kurtosis(DataBuff)
+    SigCrestFactor = Max / SigRMS
+    SigShapeFactor = SigRMS / MeanAbs
+    SigImpulseFactor = Max / MeanAbs
+    SigMarginFactor = Max / (MeanAbs**2)
+    
+    return (SigRMS, SigMean, SigMedian, SigVariance, SigSkewness, SigKurtosis, 
+            SigCrestFactor, SigShapeFactor, SigImpulseFactor, SigMarginFactor)
+
+
+def ExtractFrequencyDomainFeatures(DataBuff, BufferLen, Fs):
+    
+    fAxis_F1 = np.arange(start=0,stop=((BufferLen-1)/2), step=1)
+    fAxis_F1 = fAxis_F1*(Fs/BufferLen) # creates freq axis
+
+    DataFFT = np.abs(np.fft.fft(DataBuff))
+    DataFFT = DataFFT[:int(BufferLen/2)] # pega apenas parte positiva da fft
+    peaks, props = find_peaks(DataFFT, height=1, distance=10, prominence=1)
+    NumPks = len(peaks)
+    
+    #print(f'Num Peaks: {len(peaks)}')
+
+    indx_pks = np.argsort(props["peak_heights"]) # organiza o indice dos picos de maneira crescente 
+                                                 # ultimo indice == indice do maior pico
+
+    if NumPks == 0:
+        pks1, pks2, pks3 = 0, 0, 0
+        locs1, locs2, locs3 = 0, 0, 0
+    elif NumPks == 1:
+        pks1, locs1 = props["peak_heights"][indx_pks[-1]], fAxis_F1[peaks[indx_pks[-1]]]
+        pks2, pks3 = 0, 0
+        locs2, locs3 = 0, 0
+    elif NumPks == 2:
+        pks1, locs1 = props["peak_heights"][indx_pks[-1]], fAxis_F1[peaks[indx_pks[-1]]]
+        pks2, locs2 = props["peak_heights"][indx_pks[-2]], fAxis_F1[peaks[indx_pks[-2]]]
+        pks3, locs3 = 0, 0
+    elif NumPks >= 3:
+        pks1, locs1 = props["peak_heights"][indx_pks[-1]], fAxis_F1[peaks[indx_pks[-1]]]
+        pks2, locs2 = props["peak_heights"][indx_pks[-2]], fAxis_F1[peaks[indx_pks[-2]]]
+        pks3, locs3 = props["peak_heights"][indx_pks[-3]], fAxis_F1[peaks[indx_pks[-3]]]
+    
+    return (pks1, pks2, pks3, locs1, locs2, locs3)
+
+
 #filtro = HighpassFilter()
 #x = np.random.randn(1000)
 #y = filtro.doFilterHP(x)
